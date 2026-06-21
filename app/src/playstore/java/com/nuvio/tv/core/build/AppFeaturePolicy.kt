ade63ce2a9aaf060bfacb7de95a7b29017a0ac5f
siriusvoid <271671657+siriusvoid@users.noreply.github.com>
2026-06-21 20:27:17 +0000

    fix: show IMDb logo in playstore builds

--- app/src/playstore/java/com/nuvio/tv/core/build/AppFeaturePolicy.kt
+++ app/src/playstore/java/com/nuvio/tv/core/build/AppFeaturePolicy.kt
@@ -6,5 +6,5 @@ object AppFeaturePolicy {
     val inAppTrailerPlaybackEnabled: Boolean = false
     val externalTrailerPlaybackEnabled: Boolean = true
     val trailerPlaybackMode: TrailerPlaybackMode = TrailerPlaybackMode.EXTERNAL
-    val imdbRatingLogoEnabled: Boolean = false
+    val imdbRatingLogoEnabled: Boolean = true
 }
